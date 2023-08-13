8fde987765a6df4d5332e3146e3930e4e5bc4592
Max <maxanjo512@gmail.com>
2023-08-13 18:51:05 +0400

    gunicorn settings

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,1 +1,3 @@
 timeout = 600
+workers = 4  # Number of Gunicorn worker processes
+bind = '0.0.0.0:5000'  # IP address and port to bind to
--- myapp.py
+++ myapp.py
@@ -265,5 +265,5 @@ def get_project_details(token):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
 

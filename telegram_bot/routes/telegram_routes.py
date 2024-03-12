5eddbd5e802e7147c3d87d509ca0a9c4193e2e76
maxanjo <maxanjo512@gmail.com>
2024-03-12 15:46:29 +0400

    routes

--- telegram_bot/routes/telegram_routes.py
+++ telegram_bot/routes/telegram_routes.py
@@ -1,20 +1,28 @@
 from flask import Blueprint
 from flask import Flask, request, jsonify
-
+import subprocess
 telegram_routes = Blueprint('telegram_routes', __name__)
 
-@telegram_routes.route("/create_bot", methods=["POST"])
-def create_bot():
-    # Get bot information from request data (assuming POST request)
-    data = request.get_json()  # Replace with your preferred method to get data
-    api_key = data.get('api_key')
-    token = data.get('token')
-
-    if not api_key or not token:
-        return jsonify({'error': 'Missing bot name or username'}), 400
-
-    from tasks import create_bot_task
-    task = create_bot_task.apply_async(args=[api_key, token])
-
-    return jsonify({'message': 'Telegram bot creation initiated', 'task_id': task.id})
+@telegram_routes.route("/start-bot", methods=["POST"])
+def start_bot():
+    try:
+        subprocess.run(['sudo', 'systemctl', 'start', 'telegram_script.service'])
+        return jsonify({'result': 'Service started successfully'}) 
+    except subprocess.CalledProcessError as e:
+        return jsonify({'error': f'Error starting service: {e.output}'}), 500
    
+@telegram_routes.route("/stop-bot", methods=["POST"])
+def stop_bot():
+    try:
+        subprocess.run(['sudo', 'systemctl', 'stop', 'telegram_script.service'])
+        return jsonify({'result': 'Service stopped successfully'}) 
+    except subprocess.CalledProcessError as e:
+        return jsonify({'error': f'Error stopping service: {e.output}'}), 500
+
+@telegram_routes.route('/service-status')
+def service_status():
+    try:
+        result = subprocess.run(['sudo', 'systemctl', 'status', 'your_service_name'], capture_output=True, text=True)
+        return jsonify({'status': result.stdout})
+    except subprocess.CalledProcessError as e:
+        return jsonify({'error': f'Error getting service status: {e.output}'}), 500
